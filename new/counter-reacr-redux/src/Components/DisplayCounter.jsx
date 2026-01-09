608d1aa083e94ff03eafe764ac60065f780a27ab
Rohit kumar <rk376895869@gmail.com>
2026-01-09 14:16:39 +0530

    add file

--- new/counter-reacr-redux/src/Components/DisplayCounter.jsx
+++
@@ -1,8 +0,0 @@
-import { useSelector } from "react-redux";
-
-const DisplayCounter = () => {
-  const counter = useSelector((store) => store.counter);
-  return <p className="lead mb-4">Counter Current Value: {counter}</p>;
-};
-
-export default DisplayCounter;
